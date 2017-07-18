Floorp-Projects/Floorp

Bug 1379533 - Support the array of addressFieldDetails and creditCardFieldDetails in FormAutofillHandler. r=lchang,steveck
MozReview-Commit-ID: DM2xilht0od

browser/extensions/formautofill/test/unit/test_collectFormFields.js
@@ -12,7 +12,7 @@ const TESTCASES = [
     document: `<form><input id="given-name"><input id="family-name">
                <input id="street-addr"><input id="city"><select id="country"></select>
                <input id='email'><input id="tel"></form>`,
-    fieldDetails: [
+    addressFieldDetails: [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "address-line1"},
@@ -21,18 +21,29 @@ const TESTCASES = [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [],
+    isValidForm: {
+      address: true,
+      creditCard: false,
+    },
     ids: ["given-name", "family-name", "street-addr", "city", "country", "email", "tel"],
   },
   {
-    description: "Form with autocomplete properties and 1 token",
-    document: `<form><input id="given-name" autocomplete="given-name">
+    description: "An address and credit card form with autocomplete properties and 1 token",
+    document: `<form>
+               <input id="given-name" autocomplete="given-name">
                <input id="family-name" autocomplete="family-name">
                <input id="street-addr" autocomplete="street-address">
                <input id="city" autocomplete="address-level2">
                <select id="country" autocomplete="country"></select>
                <input id="email" autocomplete="email">
-               <input id="tel" autocomplete="tel"></form>`,
-    fieldDetails: [
+               <input id="tel" autocomplete="tel">
+               <input id="cc-number" autocomplete="cc-number">
+               <input id="cc-name" autocomplete="cc-name">
+               <input id="cc-exp-month" autocomplete="cc-exp-month">
+               <input id="cc-exp-year" autocomplete="cc-exp-year">
+               </form>`,
+    addressFieldDetails: [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "street-address"},
@@ -41,9 +52,19 @@ const TESTCASES = [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-number"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-name"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-month"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-year"},
+    ],
+    isValidForm: {
+      address: true,
+      creditCard: true,
+    },
   },
   {
-    description: "Form with autocomplete properties and 2 tokens",
+    description: "An address form with autocomplete properties and 2 tokens",
     document: `<form><input id="given-name" autocomplete="shipping given-name">
                <input id="family-name" autocomplete="shipping family-name">
                <input id="street-addr" autocomplete="shipping street-address">
@@ -51,7 +72,7 @@ const TESTCASES = [
                <input id="country" autocomplete="shipping country">
                <input id='email' autocomplete="shipping email">
                <input id="tel" autocomplete="shipping tel"></form>`,
-    fieldDetails: [
+    addressFieldDetails: [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "street-address"},
@@ -60,6 +81,11 @@ const TESTCASES = [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [],
+    isValidForm: {
+      address: true,
+      creditCard: false,
+    },
   },
   {
     description: "Form with autocomplete properties and profile is partly matched",
@@ -70,7 +96,7 @@ const TESTCASES = [
                <select autocomplete="shipping country"></select>
                <input id='email' autocomplete="shipping email">
                <input id="tel" autocomplete="shipping tel"></form>`,
-    fieldDetails: [
+    addressFieldDetails: [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "street-address"},
@@ -79,6 +105,59 @@ const TESTCASES = [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [],
+    isValidForm: {
+      address: true,
+      creditCard: false,
+    },
+  },
+  {
+    description: "It's a valid address and credit card form.",
+    document: `<form>
+               <input id="given-name" autocomplete="shipping given-name">
+               <input id="family-name" autocomplete="shipping family-name">
+               <input id="street-addr" autocomplete="shipping street-address">
+               <input id="city" autocomplete="shipping address-level2">
+               <input id="cc-number" autocomplete="shipping cc-number">
+               </form>`,
+    addressFieldDetails: [
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "family-name"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "street-address"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "address-level2"},
+    ],
+    creditCardFieldDetails: [
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "cc-number"},
+    ],
+    isValidForm: {
+      address: true,
+      creditCard: true,
+    },
+  },
+  {
+    description: "It's an invalid address and credit form.",
+    document: `<form>
+               <input id="given-name" autocomplete="shipping given-name">
+               <input autocomplete="shipping address-level2">
+               <select autocomplete="shipping country"></select>
+               <input id="cc-name" autocomplete="cc-name">
+               <input id="cc-exp-month" autocomplete="cc-exp-month">
+               <input id="cc-exp-year" autocomplete="cc-exp-year">
+               </form>`,
+    addressFieldDetails: [
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "address-level2"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "country"},
+    ],
+    creditCardFieldDetails: [
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-name"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-month"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-year"},
+    ],
+    isValidForm: {
+      address: false,
+      creditCard: false,
+    },
   },
 ];
 
@@ -93,7 +172,10 @@ for (let tc of TESTCASES) {
       let form = doc.querySelector("form");
       let formLike = FormLikeFactory.createFromForm(form);
 
-      testcase.fieldDetails.forEach((detail, index) => {
+      Array.of(
+        ...testcase.addressFieldDetails,
+        ...testcase.creditCardFieldDetails
+      ).forEach((detail, index) => {
         let elementRef;
         if (testcase.ids && testcase.ids[index]) {
           elementRef = doc.getElementById(testcase.ids[index]);
@@ -106,13 +188,21 @@ for (let tc of TESTCASES) {
 
       handler.collectFormFields();
 
-      handler.fieldDetails.forEach((detail, index) => {
-        Assert.equal(detail.section, testcase.fieldDetails[index].section);
-        Assert.equal(detail.addressType, testcase.fieldDetails[index].addressType);
-        Assert.equal(detail.contactType, testcase.fieldDetails[index].contactType);
-        Assert.equal(detail.fieldName, testcase.fieldDetails[index].fieldName);
-        Assert.equal(detail.elementWeakRef.get(), testcase.fieldDetails[index].elementWeakRef.get());
-      });
+      function verifyDetails(handlerDetails, testCaseDetails) {
+        handlerDetails.forEach((detail, index) => {
+          Assert.equal(detail.section, testCaseDetails[index].section);
+          Assert.equal(detail.addressType, testCaseDetails[index].addressType);
+          Assert.equal(detail.contactType, testCaseDetails[index].contactType);
+          Assert.equal(detail.fieldName, testCaseDetails[index].fieldName);
+          Assert.equal(detail.elementWeakRef.get(), testCaseDetails[index].elementWeakRef.get());
+        });
+      }
+
+      verifyDetails(handler.addressFieldDetails, testcase.addressFieldDetails);
+      verifyDetails(handler.creditCardFieldDetails, testcase.creditCardFieldDetails);
+
+      Assert.equal(handler.isValidAddressForm, testcase.isValidForm.address, "Valid Address Form Checking");
+      Assert.equal(handler.isValidCreditCardForm, testcase.isValidForm.creditCard, "Valid Credit Card Form Checking");
     });
   })();
 }

weilonge/gecko

browser/extensions/formautofill/test/unit/test_collectFormFields.js
@@ -12,7 +12,7 @@ const TESTCASES = [
     document: `<form><input id="given-name"><input id="family-name">
                <input id="street-addr"><input id="city"><select id="country"></select>
                <input id='email'><input id="tel"></form>`,
-    fieldDetails: [
+    addressFieldDetails: [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "address-line1"},
@@ -21,18 +21,29 @@ const TESTCASES = [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [],
+    isValidForm: {
+      address: true,
+      creditCard: false,
+    },
     ids: ["given-name", "family-name", "street-addr", "city", "country", "email", "tel"],
   },
   {
-    description: "Form with autocomplete properties and 1 token",
-    document: `<form><input id="given-name" autocomplete="given-name">
+    description: "An address and credit card form with autocomplete properties and 1 token",
+    document: `<form>
+               <input id="given-name" autocomplete="given-name">
                <input id="family-name" autocomplete="family-name">
                <input id="street-addr" autocomplete="street-address">
                <input id="city" autocomplete="address-level2">
                <select id="country" autocomplete="country"></select>
                <input id="email" autocomplete="email">
-               <input id="tel" autocomplete="tel"></form>`,
-    fieldDetails: [
+               <input id="tel" autocomplete="tel">
+               <input id="cc-number" autocomplete="cc-number">
+               <input id="cc-name" autocomplete="cc-name">
+               <input id="cc-exp-month" autocomplete="cc-exp-month">
+               <input id="cc-exp-year" autocomplete="cc-exp-year">
+               </form>`,
+    addressFieldDetails: [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "street-address"},
@@ -41,9 +52,19 @@ const TESTCASES = [
       {"section": "", "addressType": "", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-number"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-name"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-month"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-year"},
+    ],
+    isValidForm: {
+      address: true,
+      creditCard: true,
+    },
   },
   {
-    description: "Form with autocomplete properties and 2 tokens",
+    description: "An address form with autocomplete properties and 2 tokens",
     document: `<form><input id="given-name" autocomplete="shipping given-name">
                <input id="family-name" autocomplete="shipping family-name">
                <input id="street-addr" autocomplete="shipping street-address">
@@ -51,7 +72,7 @@ const TESTCASES = [
                <input id="country" autocomplete="shipping country">
                <input id='email' autocomplete="shipping email">
                <input id="tel" autocomplete="shipping tel"></form>`,
-    fieldDetails: [
+    addressFieldDetails: [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "street-address"},
@@ -60,6 +81,11 @@ const TESTCASES = [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [],
+    isValidForm: {
+      address: true,
+      creditCard: false,
+    },
   },
   {
     description: "Form with autocomplete properties and profile is partly matched",
@@ -70,7 +96,7 @@ const TESTCASES = [
                <select autocomplete="shipping country"></select>
                <input id='email' autocomplete="shipping email">
                <input id="tel" autocomplete="shipping tel"></form>`,
-    fieldDetails: [
+    addressFieldDetails: [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "family-name"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "street-address"},
@@ -79,6 +105,59 @@ const TESTCASES = [
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "email"},
       {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "tel"},
     ],
+    creditCardFieldDetails: [],
+    isValidForm: {
+      address: true,
+      creditCard: false,
+    },
+  },
+  {
+    description: "It's a valid address and credit card form.",
+    document: `<form>
+               <input id="given-name" autocomplete="shipping given-name">
+               <input id="family-name" autocomplete="shipping family-name">
+               <input id="street-addr" autocomplete="shipping street-address">
+               <input id="city" autocomplete="shipping address-level2">
+               <input id="cc-number" autocomplete="shipping cc-number">
+               </form>`,
+    addressFieldDetails: [
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "family-name"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "street-address"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "address-level2"},
+    ],
+    creditCardFieldDetails: [
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "cc-number"},
+    ],
+    isValidForm: {
+      address: true,
+      creditCard: true,
+    },
+  },
+  {
+    description: "It's an invalid address and credit form.",
+    document: `<form>
+               <input id="given-name" autocomplete="shipping given-name">
+               <input autocomplete="shipping address-level2">
+               <select autocomplete="shipping country"></select>
+               <input id="cc-name" autocomplete="cc-name">
+               <input id="cc-exp-month" autocomplete="cc-exp-month">
+               <input id="cc-exp-year" autocomplete="cc-exp-year">
+               </form>`,
+    addressFieldDetails: [
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "given-name"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "address-level2"},
+      {"section": "", "addressType": "shipping", "contactType": "", "fieldName": "country"},
+    ],
+    creditCardFieldDetails: [
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-name"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-month"},
+      {"section": "", "addressType": "", "contactType": "", "fieldName": "cc-exp-year"},
+    ],
+    isValidForm: {
+      address: false,
+      creditCard: false,
+    },
   },
 ];
 
@@ -93,7 +172,10 @@ for (let tc of TESTCASES) {
       let form = doc.querySelector("form");
       let formLike = FormLikeFactory.createFromForm(form);
 
-      testcase.fieldDetails.forEach((detail, index) => {
+      Array.of(
+        ...testcase.addressFieldDetails,
+        ...testcase.creditCardFieldDetails
+      ).forEach((detail, index) => {
         let elementRef;
         if (testcase.ids && testcase.ids[index]) {
           elementRef = doc.getElementById(testcase.ids[index]);
@@ -106,13 +188,21 @@ for (let tc of TESTCASES) {
 
       handler.collectFormFields();
 
-      handler.fieldDetails.forEach((detail, index) => {
-        Assert.equal(detail.section, testcase.fieldDetails[index].section);
-        Assert.equal(detail.addressType, testcase.fieldDetails[index].addressType);
-        Assert.equal(detail.contactType, testcase.fieldDetails[index].contactType);
-        Assert.equal(detail.fieldName, testcase.fieldDetails[index].fieldName);
-        Assert.equal(detail.elementWeakRef.get(), testcase.fieldDetails[index].elementWeakRef.get());
-      });
+      function verifyDetails(handlerDetails, testCaseDetails) {
+        handlerDetails.forEach((detail, index) => {
+          Assert.equal(detail.section, testCaseDetails[index].section);
+          Assert.equal(detail.addressType, testCaseDetails[index].addressType);
+          Assert.equal(detail.contactType, testCaseDetails[index].contactType);
+          Assert.equal(detail.fieldName, testCaseDetails[index].fieldName);
+          Assert.equal(detail.elementWeakRef.get(), testCaseDetails[index].elementWeakRef.get());
+        });
+      }
+
+      verifyDetails(handler.addressFieldDetails, testcase.addressFieldDetails);
+      verifyDetails(handler.creditCardFieldDetails, testcase.creditCardFieldDetails);
+
+      Assert.equal(handler.isValidAddressForm, testcase.isValidForm.address, "Valid Address Form");
+      Assert.equal(handler.isValidCreditCardForm, testcase.isValidForm.creditCard, "Valid Credit Card Form");
     });
   })();
 }